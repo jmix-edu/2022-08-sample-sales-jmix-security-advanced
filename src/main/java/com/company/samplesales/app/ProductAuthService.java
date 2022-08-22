2b17236b8dc17cb0afe2e214022c9d371b8caf52
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-08-22 16:09:00 +0400

    Complete KeyCloak REST

---
+++ src/main/java/com/company/samplesales/app/ProductAuthService.java
@@ -0,0 +1,32 @@
+package com.company.samplesales.app;
+
+import io.jmix.core.UnconstrainedDataManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/authenticated/product")
+public class ProductAuthService {
+
+    private UnconstrainedDataManager unconstrainedDataManager;
+
+    public ProductAuthService(UnconstrainedDataManager unconstrainedDataManager) {
+        this.unconstrainedDataManager = unconstrainedDataManager;
+    }
+    
+    @GetMapping("/list")
+    public ResponseEntity<List<String>> list() {
+        List<String> productNames = unconstrainedDataManager.loadValues("select p.name from sales_Product p order by p.name")
+                .property("name")
+                .list()
+                .stream().map(row -> (String) row.getValue("name"))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(productNames, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/company/samplesales/app/ProductPublicService.java
@@ -0,0 +1,33 @@
+package com.company.samplesales.app;
+
+import io.jmix.core.UnconstrainedDataManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/public/product")
+public class ProductPublicService {
+
+    private UnconstrainedDataManager unconstrainedDataManager;
+
+    public ProductPublicService(UnconstrainedDataManager unconstrainedDataManager) {
+        this.unconstrainedDataManager = unconstrainedDataManager;
+    }
+    
+    @GetMapping("/list")
+    public ResponseEntity<List<String>> list() {
+        List<String> productNames = unconstrainedDataManager.loadValues("select p.name from sales_Product p " +
+                        "where p.special IS NULL or p.special = false order by p.name")
+                .property("name")
+                .list()
+                .stream().map(row -> (String) row.getValue("name"))
+                .collect(Collectors.toList());
+        return new ResponseEntity<>(productNames, HttpStatus.OK);
+    }
+}
